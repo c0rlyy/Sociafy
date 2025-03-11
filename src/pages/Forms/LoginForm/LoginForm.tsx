1a9830c9fa31d1dd05013ee325fdd6edece8ddf6
Daniel Niedzielski <daniel.niedziels@wp.pl>
2025-03-11 11:18:49 +0100

    fix: login page ui fix

--- src/pages/Forms/LoginForm/LoginForm.tsx
+++ src/pages/Forms/LoginForm/LoginForm.tsx
@@ -5,7 +5,7 @@ import SociafyLogo from "../../../assets/SVG 2/Sociafy.svg";
 import { loginForm, useAuth } from "../../../store/AuthContext";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import { ZodError, z } from "zod";
 type loginFormScreen = {
   mdScreen: boolean;
 };
@@ -19,8 +19,6 @@ type loginType = {
 };
 const LoginForm: React.FC<loginFormScreen> = ({ mdScreen }) => {
   const { loginAction } = useAuth();
-  const [formValidation, setformValidation] = useState<boolean>(false);
-  const [isTyping, setIsTyping] = useState(false);
   const loginSchema = z.object({
     username: z.string().min(3),
     password: z.string().min(8),
@@ -33,9 +31,9 @@ const LoginForm: React.FC<loginFormScreen> = ({ mdScreen }) => {
     resolver: zodResolver(loginSchema),
   });
   const navigation = useNavigate();
-  // Validation before redirecting to /MainPage
   const onSubmit = async (data: loginForm) => {
     try {
+      console.log(isSubmitting)
       const token = await loginAction(data);
       if (token) {
         navigation("/MainPage");
@@ -43,19 +41,22 @@ const LoginForm: React.FC<loginFormScreen> = ({ mdScreen }) => {
       } else {
         throw new Error("Sorry, something went wrong");
       }
-    } catch (error: any) {
-      console.error(error?.message);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new Error(`Check if your credentials met requirements: ${error}`)
+      }
+      else {
+        throw new Error(error)
+      }
     }
   };
   return (
     <div
-      className={` ${
-        mdScreen ? "col-[3/4]" : "col-[1/-1]"
-      } flex h-full w-screen flex-col items-center gap-3 rounded-lg bg-[#F3F4F6] p-3 shadow-md  shadow-[#329CE5] lg:h-auto lg:w-3/4 lg:justify-start `}
+      className={" lg:col-[3/4] md:col-[3/3] sm:col-[1/-1] col-[1/-1] flex h-1/2 w-screen flex-col items-center gap-3 rounded-lg bg-[#F3F4F6] p-3 shadow-md  shadow-[#329CE5] lg:h-1/2  lg:w-full lg:justify-start "}
     >
       <form onSubmit={handleSubmit(onSubmit)} className={login.loginForm}>
         <picture className={login.logoContainer}>
-          <img className={login.logo} src={`${SociafyLogo}`} alt="" />
+          <img className={login.logo} src={`${SociafyLogo}`} alt="sociafy-logo" />
         </picture>
         <div className={login.inputs}>
           <div className={login.loginField}>
@@ -64,7 +65,7 @@ const LoginForm: React.FC<loginFormScreen> = ({ mdScreen }) => {
               type="text"
               {...register("username")}
             />
-            <label htmlFor="email" className={login.label}>
+            <label htmlFor="email">
               Username
             </label>
             {errors.username && (
@@ -77,34 +78,35 @@ const LoginForm: React.FC<loginFormScreen> = ({ mdScreen }) => {
               type="password"
               {...register("password")}
             />
-            <label className={login.label} htmlFor="password">
+            <label htmlFor="password">
               Password
             </label>
             {errors.password && (
               <p className="text-sm text-red-500">{errors.password.message}</p>
             )}
           </div>
-          <div className="self-center justify-self-center">
-            <input
-              className="w-full bg-[#009fe3] p-2 font-bold text-white"
+          <div className="flex  w-full gap-2  p-4 justify-center ">
+            <button
+              className={` ${isSubmitting ? "bg-opacity-50": "bg-opacity-100"} bg-[#009fe3] w-1/2 px-4 py-2 rounded-md font-normal text-white text-md hover:bg-[#0080b5] transition duration-300 border`}
               type="submit"
               value={"Sign In"}
               disabled={isSubmitting}
-            />
+            >
+              Sign In
+          </button>
+            <button
+              type="submit"
+              className="w-1/2 cursor-pointer rounded-md bg-[#33BFFF] hover:bg-[#005F8A] p-2 text-center text-md font-normal hover:bg-[#A6A6A6] transition duration-300 text-white"
+              value={"Sign Up"}
+            >
+          <Link to={"/Register"}>
+              Sign Up
+          </Link>
+            </button>
           </div>
         </div>
       </form>
-      <div className={login.loginButtonsContainer}>
-        <Link to={"/Register"}>
-          <button
-            type="submit"
-            className="w-full cursor-pointer rounded-md bg-[#BFBFBF] p-2 text-center text-[clamp(.8rem,1.3vw,1rem)] font-bold text-white"
-            value={"Sign Up"}
-          >
-            Sign Up
-          </button>
-        </Link>
-      </div>
+
     </div>
   );
 };
