e50be55bc31236d610e4e51d28783cbe3cb78902
Daniel Niedzielski <daniel.niedziels@wp.pl>
2025-03-25 11:27:39 +0100

    hotfix

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -5,8 +5,8 @@ export const loginSchema = z.object({
 });
 export const registerSchema = z.object({
   email: z.string().trim().min(1, {message:"Email field is required"}).email({ message:"Please enter valid email"}),
-  username: z.string().min(1, { message: "Username field is required" }).min(3, { message: "Username is too short" }).max(32, {message:"Username is too long"}),
-  password: z.string().min(1,{message:"Password field is required"}).min(8, { message: "Password must contain at least 8 characters" })
+  username: z.string().trim().min(1, { message: "Username field is required" }).min(3, { message: "Username is too short" }).max(32, {message:"Username is too long"}),
+  password: z.string().trim().min(1,{message:"Password field is required"}).min(8, { message: "Password must contain at least 8 characters" })
     .regex(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character"),
   repeatPassword: z.string().trim().min(1, {message:"Repeat password field is required"}),
   terms: z.boolean({message:"Please accept terms of service and privacy policy."})
